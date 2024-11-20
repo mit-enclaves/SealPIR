bf3b266db803263afa6c203261c21d6d9be29add
Jules Drean <drean@mit.edu>
2024-11-20 23:22:50 +0000

    Add measurements for benchmark

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/drean/SealPIR
+# Build directory: /home/drean/SealPIR
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
